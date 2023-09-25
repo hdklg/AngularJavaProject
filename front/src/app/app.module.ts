7f49404ecaa7de07a03a13e45578c37eef3c64ce
hdklg <shubhampatil230499@gmail.com>
2023-09-25 21:31:40 +0530

    new home screen

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,17 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './homepage/home-page/home-page.component';
 import { HeaderComponent } from './homepage/header/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './homepage/footer/footer.component';
+import { BodyComponent } from './homepage/body/body.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
+    
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
